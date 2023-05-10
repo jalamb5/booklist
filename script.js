4055ea85002e03f1be3dabb8ed3eb9acc1d59f25
Justin Lamb <justinlamb@me.com>
2023-05-10 12:18:11 -0700

    test of myLib insertion

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let myLibrary = [];
+let myLibrary = ["Book 1", "Book 2", "Book 3", "Book 4"];
 
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
-  this.info = function() {
+  this.info = function () {
     return title + author + ", " + pages + " pages"
   }
 }
@@ -13,3 +13,31 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() {
 
 }
+
+function showBooks() {
+  let bookView = document.getElementById("showBooks");
+
+  for (let i of myLibrary) {
+    let newBook = document.createTextNode(i);
+    bookView.appendChild(newBook);
+  }
+}
+
+showBooks()
+
+// document.body.onload = addElement;
+
+// function addElement() {
+//   // create a new div element
+//   const newDiv = document.createElement("div");
+
+//   // and give it some content
+//   const newContent = document.createTextNode("Hi there and greetings!");
+
+//   // add the text node to the newly created div
+//   newDiv.appendChild(newContent);
+
+//   // add the newly created element and its content into the DOM
+//   const currentDiv = document.getElementById("div1");
+//   document.body.insertBefore(newDiv, currentDiv);
+// }
